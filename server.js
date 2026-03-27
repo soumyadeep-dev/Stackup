7c3be0499709950048e8b4adaf5599a72cd7e55b
soumyadeep-dev <111150506+soumyadeep-dev@users.noreply.github.com>
2026-03-28 01:04:43 +0530

    made changes for render

--- server.js
+++ server.js
@@ -31,11 +31,8 @@ const upload = multer({ storage });
 
 // 🗄️ PostgreSQL
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 // ─────────────────────────────────────────
